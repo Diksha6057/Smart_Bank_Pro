4e24109002b7b46c31d50c7ac2280e49ddbf3aec
ATG-WAR <sharmaarsh3001@gmail.com>
2026-04-05 07:00:14 +0530

    fixed errors

--- Smart_Bank_Pro/CurrentAccount.h
+++ Smart_Bank_Pro/CurrentAccount.h
@@ -1,20 +1,16 @@
 #ifndef CURRENTACCOUNT_H
 #define CURRENTACCOUNT_H
-
 #include "Account.h"
-
 class CurrentAccount : public Account
 {
 private:
     double overdraftLimit;
-
 public:
     CurrentAccount(int accNo, string name, double bal, double limit);
     void withdraw(double amount) override;
     void calculateInterest();
     void applyServiceCharge(double charge);
     void display() const override;
-    void accountType() override;   // ✅ REQUIRED
+    void accountType() override;   
 };
-
 #endif
